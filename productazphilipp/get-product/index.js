ce74b84676d439fa8a0cc1bbcea02ec8c8fd7c71
pschengel <136333034+pschengel@users.noreply.github.com>
2023-06-28 13:34:51 +0200

    Update index.js

--- productazphilipp/get-product/index.js
+++ productazphilipp/get-product/index.js
@@ -3,8 +3,8 @@ const { CosmosClient } = require("@azure/cosmos")
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const endpoint = "https://azure-db-philipp.documents.azure.com:443/";
-    const key = "l31FevtivlF067vIjf2Wm0C0eJW52cwmvDh7i7G4MAA7f4fMAyCepQqBfgyGVv9oKnsDP4UOo3qcACDbkXBIRg==";
+    const endpoint = "";
+    const key = "";
     const client = new CosmosClient({ endpoint, key });
 
     const { database } =  await client.databases.createIfNotExists({ id: "MyDatabase" });
